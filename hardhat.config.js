e341961ec6c42b2afe8d6dbe1294db13245b9358
jayteemoney <jethroirmiya@gmail.com>
2025-04-27 18:10:14 +0000

    staking

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: process.env.INFURA_API_URL,
+      url: process.env.ALCHEMY_SEPOLIA_API_URL,
     
 
       accounts: [process.env.ACCOUNT_PRIVATE_KEY],
