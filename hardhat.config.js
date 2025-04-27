a1bbc0d9f14a248ea2bcf114c2b85829221a5fab
jayteemoney <jethroirmiya@gmail.com>
2025-04-27 19:20:49 +0000

    staking

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.27",
   networks: {
     sepolia: {
       url: process.env.ALCHEMY_SEPOLIA_API_URL,
